b4103368f9110ea622ce8fe6ee73090ec4fb2b7e
AbdulHalim <3010710249@kayseri.edu.tr>
2023-03-07 12:17:09 +0300

    LibGdx-Fruit-Ninja-Clone-Java
    I'm made fruit ninja clone with using libgdx java.

---
+++ FruitNinjaClone/android/src/com/haaliron/fruitninjaclone/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.haaliron.fruitninjaclone;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.haaliron.fruitninjaclone.FruitNinja;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FruitNinja(), config);
+	}
+}
